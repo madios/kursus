6fe4459c1d93e75b30a4c97a94ed76b77bba4743
madios <peterammundsen@gmail.com>
2021-11-25 08:54:52 +0100

    first commit

---
+++ kursus/rod/rod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
